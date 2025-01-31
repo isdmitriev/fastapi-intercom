4c7270ec5176e0c39fd55bf7a12f0576a7d3c574
isdmitriev <isdmitriev2@gmail.com>
2025-01-31 11:18:08 +0200

    add_cd

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ async def root():
     return Response(status_code=status.HTTP_200_OK, content="start app")
 
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
