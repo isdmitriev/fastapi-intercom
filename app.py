a864b7b0ff9420758ff570d2590e1353b7db52cb
isdmitriev <isdmitriev2@gmail.com>
2025-02-02 15:40:51 +0200

    31.01.2025(add_kubernetes)

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ import uvicorn
 from typing import Dict
 import motor.motor_asyncio
 from services.web_hook_processor import WebHookProcessor
+from services.mongodb_service import MongodbService
 
 app = FastAPI()
 
 processor: WebHookProcessor = WebHookProcessor()
+mongodb_service: MongodbService = MongodbService()
 
 
 @app.post("/webhook/message")
 async def get_message(request: Request):
     try:
         payload: Dict = await request.json()
+        await mongodb_service.add_document_to_collection('intercom_app', 'event_logs', payload)
+
         topic: str = payload.get("topic", "")
         await processor.process_message(topic, payload)
         return Response(status_code=status.HTTP_200_OK)
