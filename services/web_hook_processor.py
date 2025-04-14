3a5f18a020e1a2ac3fbc50ec881d6be882ac5f34
isdmitriev <isdmitriev2@gmail.com>
2025-04-14 10:53:56 +0300

    14.04.2025(1)

--- services/web_hook_processor.py
+++ services/web_hook_processor.py
@@ -28,14 +28,14 @@ import time
 class WebHookProcessor:
 
     def __init__(
-        self,
-        mongo_db_service: MongodbService,
-        openai_service: OpenAIService,
-        intercom_service: IntercomAPIService,
-        conversation_parts_service: ConversationPartsService,
-        messages_cache_service: MessagesCache,
-        translations_service: OpenAITranslatorService,
-        es_service: ESService,
+            self,
+            mongo_db_service: MongodbService,
+            openai_service: OpenAIService,
+            intercom_service: IntercomAPIService,
+            conversation_parts_service: ConversationPartsService,
+            messages_cache_service: MessagesCache,
+            translations_service: OpenAITranslatorService,
+            es_service: ESService,
     ):
         self.mongo_db_service = mongo_db_service
         self.openai_service = openai_service
@@ -278,7 +278,7 @@ class WebHookProcessor:
             raise e
 
     async def send_admin_note_async(
-        self, conversation_id: str, message: str, message_language
+            self, conversation_id: str, message: str, message_language
     ):
         admin_id: str = "8024055"
         if message_language == "Hindi":
@@ -330,19 +330,19 @@ class WebHookProcessor:
         one: str = possible_interpritations[0]
         two: str = possible_interpritations[1]
         note: str = (
-            "translated: "
-            + message.translated_text
-            + "\n"
-            + message.context_analysis
-            + "\n"
-            + one
-            + "\n"
-            + two
+                "translated: "
+                + message.translated_text
+                + "\n"
+                + message.context_analysis
+                + "\n"
+                + one
+                + "\n"
+                + two
         )
         return note
 
     async def save_request_info(
-        self, status: str, execution_time: float, event_type: str
+            self, status: str, execution_time: float, event_type: str
     ):
         request_info: RequestInfo = RequestInfo(
             status=status,
@@ -358,7 +358,7 @@ class WebHookProcessor:
         # es_client.add_document(index_name="requests", document=request_info.dict())
 
     async def save_first_message_to_cache(
-        self, conversation_id: str, message: ConversationMessage
+            self, conversation_id: str, message: ConversationMessage
     ):
         messages: ConversationMessages = ConversationMessages(messages=[message])
         self.messages_cache_service.set_conversation_messages(
@@ -366,10 +366,10 @@ class WebHookProcessor:
         )
 
     async def save_first_message_to_cache_2(
-        self,
-        conversation_id: str,
-        message: ConversationMessage,
-        analyzed_message: UserMessage,
+            self,
+            conversation_id: str,
+            message: ConversationMessage,
+            analyzed_message: UserMessage,
     ):
         translated_message: str = analyzed_message.translated_text
         if analyzed_message.status == "no_error":
@@ -380,7 +380,7 @@ class WebHookProcessor:
             )
 
     async def save_message_to_cache(
-        self, conversation_id: str, message: ConversationMessage
+            self, conversation_id: str, message: ConversationMessage
     ):
         all_conversation_messages = (
             self.messages_cache_service.get_conversation_messages(
@@ -767,12 +767,12 @@ class WebHookProcessor:
             raise e
 
     async def send_admin_reply_message(
-        self,
-        user: User,
-        conversation_id: str,
-        admin_id: str,
-        message: str,
-        target_language: str,
+            self,
+            user: User,
+            conversation_id: str,
+            admin_id: str,
+            message: str,
+            target_language: str,
     ):
         conv_message: ConversationMessage = ConversationMessage(
             conversation_id=conversation_id,
